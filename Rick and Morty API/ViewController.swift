745ee3aec2cfffa95a0750307603582714597f59
sofiigorevna <sofia.konchencko@yandex.ru>
2024-01-18 15:38:55 +0300

    Initial Commit

---
+++ Rick and Morty API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty API
+//
+//  Created by 1234 on 18.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

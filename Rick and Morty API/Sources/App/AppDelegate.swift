a39fa56ca32aed414241ddb504c32c97479966fe
sofiigorevna <sofia.konchencko@yandex.ru>
2024-01-18 16:28:59 +0300

    Preparing the project for work.

---
+++ Rick and Morty API/Sources/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Rick and Morty API
+//
+//  Created by 1234 on 18.01.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+

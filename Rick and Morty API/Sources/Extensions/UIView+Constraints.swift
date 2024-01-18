84a2b0faf13e4bd315afc3931fca8947fbe865f7
sofiigorevna <sofia.konchencko@yandex.ru>
2024-01-18 20:06:48 +0300

    Create Animated Gif LaunchScree.

---
+++ Rick and Morty API/Sources/Extensions/UIView+Constraints.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Constraints.swift
+//  Rick and Morty API
+//
+//  Created by 1234 on 18.01.2024.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
+}
--- Rick and Morty API/Sources/Views/ViewController.swift
+++ Rick and Morty API/Sources/Views/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SwiftyGif
 
 class ViewController: UIViewController {
 
+    let logoAnimationView = LogoAnimationView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        view.addSubview(logoAnimationView)
+        logoAnimationView.pinEdgesToSuperView()
+        logoAnimationView.logoGifImageView.delegate = self
+        setupHierarhy()
+        setupLayout()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logoAnimationView.logoGifImageView.startAnimatingGif()
     }
 
 
+    private func setupHierarhy() {
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+           
+        ])
+    }
+}
+
+extension ViewController: SwiftyGifDelegate {
+    func gifDidStop(sender: UIImageView) {
+        logoAnimationView.isHidden = true
+    }
 }
 
